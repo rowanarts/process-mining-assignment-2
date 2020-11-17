c41af26644ec8602efe42a9474e4a8ee602c499a
RowanArts <rmjarts@gmail.com>
2020-11-17 09:49:32 +0100

    changes to Q1

--- Q1/utils.py
+++ Q1/utils.py
@@ -6,14 +6,14 @@ from typing import Any
 
 import pandas as pd
 import pickle
-import py4pm
+import pm4py
 
 # input
 PATH_LOG = '../dataset/BPI Challenge 2018.xes'  # original log FILE.
 PATH_CSV = '../dataset/BPI Challenge 2018.csv'  # original log, but saveD as csv via Disco.
 
 # transformed files, but still got all original data
-PATH_DF_PARQUET = '../dataset/BPI Challenge_df.parquet'  # original log, but saved as .parquet file since it loads much faster.
+PATH_DF_PARQUET = '../dataset/BPI_Challenge_df.parquet'  # original log, but saved as .parquet file since it loads much faster.
 PATH_LOG_PKL = '../dataset/BPI Challenge 2018'  # original log, loaded as csv/pickle, executed some data transformations and saved as pickle.
 
 # filtered files
@@ -31,7 +31,7 @@ def save_parquet_original(frame: pd.DataFrame) -> None:
     frame.to_parquet(PATH_DF_PARQUET)
 
 
-def save_log_original(log_object: py4pm) -> None:
+def save_log_original(log_object: pm4py) -> None:
     return _save_pickle(log_object, PATH_LOG_PKL)
     
 
@@ -39,7 +39,7 @@ def save_parquet_filtered(frame: pd.DataFrame) -> None:
     frame.to_parquet(PATH_DF_PARQUET_FILTERED)
     
 
-def save_log_filtered(log_object: py4pm) -> None:
+def save_log_filtered(log_object: pm4py) -> None:
     return _save_pickle(log_object, PATH_LOG_PKL_FILTERED)
 
 
